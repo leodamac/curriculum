1fc69ad5dca55e2da99297026a410a895b7b0bdf
Leonardo Macias <105889369+leodamac@users.noreply.github.com>
2025-05-19 18:38:22 +0000

    Actualizar enlaces de imagen en los datos de proyectos

--- scripts/main.js
+++ scripts/main.js
@@ -51,7 +51,7 @@ const proyectosData = [
     titulo: "Sistema de simulación de envío de datos entre diferentes capas",
     descripcion: "Aplicación que permite visualizar el funcionamiento de las capas del modelo OSI.",
     tecnologias: "Java",
-    imagen: "https://private-user-images.githubusercontent.com/105889369/443815553-aaedf306-183d-405d-a0d0-ada5f48f2296.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDc2Nzk4OTYsIm5iZiI6MTc0NzY3OTU5NiwicGF0aCI6Ii8xMDU4ODkzNjkvNDQzODE1NTUzLWFhZWRmMzA2LTE4M2QtNDA1ZC1hMGQwLWFkYTVmNDhmMjI5Ni5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNTE5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDUxOVQxODMzMTZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yMDgyMzg0ZjI4YjgzZjQzZTI3MzVkYWRmNjEzYjc3YjBmZWM5Y2RlODcwNzllOGEyY2NkOGUwZDAwZGIwMjg2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.ztAfh0M4qUyd0mFOoUHeiIJMcv5Ga3ltA60DsGDquCU",
+    imagen: "https://raw.githubusercontent.com/leodamac/Proyecto-Redes/main/Grupo7_RedesDeDatos/src/main/resources/tcpip.jpg",
     enlace: "https://github.com/leodamac/Proyecto-Redes"
   },
   {
@@ -59,7 +59,7 @@ const proyectosData = [
     titulo: "Sistema de Marcación de Asistencia",
     descripcion: "Plataforma para gestionar asistencias.",
     tecnologias: "Firebase, TypeScript, React, HTML, CSS",
-    imagen: "https://private-user-images.githubusercontent.com/105889369/443815553-aaedf306-183d-405d-a0d0-ada5f48f2296.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDcyNTE2ODIsIm5iZiI6MTc0NzI1MTM4MiwicGF0aCI6Ii8xMDU4ODkzNjkvNDQzODE1NTUzLWFhZWRmMzA2LTE4M2QtNDA1ZC1hMGQwLWFkYTVmNDhmMjI5Ni5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNTE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDUxNFQxOTM2MjJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0zYTc5N2YwM2I2YmVmNzJlZjc0MjcxYWQ0ODBjMGJlMGEzYmU1NjUyZWJhNjAzYWVmYWU1OTI0YzEwZGFiNjBlJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.TkBOJlJCFLWY2mZX5Ye-UDfuBCvSiigFou8eOa-0H3U",
+    imagen: "https://private-user-images.githubusercontent.com/105889369/443815553-aaedf306-183d-405d-a0d0-ada5f48f2296.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDc2Nzk4OTYsIm5iZiI6MTc0NzY3OTU5NiwicGF0aCI6Ii8xMDU4ODkzNjkvNDQzODE1NTUzLWFhZWRmMzA2LTE4M2QtNDA1ZC1hMGQwLWFkYTVmNDhmMjI5Ni5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNTE5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDUxOVQxODMzMTZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yMDgyMzg0ZjI4YjgzZjQzZTI3MzVkYWRmNjEzYjc3YjBmZWM5Y2RlODcwNzllOGEyY2NkOGUwZDAwZGIwMjg2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.ztAfh0M4qUyd0mFOoUHeiIJMcv5Ga3ltA60DsGDquCU",
     enlace: "https://github.com/leodamac/Voluntariado"
   },
   {
